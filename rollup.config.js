2d6d62989b497b946b114b1c4c19fd03ecd1e907
syd18b <snb5728@psu.edu>
2024-10-17 12:24:28 -0400

    copy

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,17 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
-import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
-import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import esbuild from 'rollup-plugin-esbuild';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
+import { importMetaAssets } from "@web/rollup-plugin-import-meta-assets";
+import esbuild from "rollup-plugin-esbuild";
 
 export default {
-  input: 'index.html',
+  input: "index.html",
   output: {
-    entryFileNames: '[hash].js',
-    chunkFileNames: '[hash].js',
-    assetFileNames: '[hash][extname]',
-    format: 'es',
-    dir: 'public',
+    entryFileNames: "[hash].js",
+    chunkFileNames: "[hash].js",
+    assetFileNames: "[hash][extname]",
+    format: "es",
+    dir: "public",
   },
   preserveEntrySignatures: false,
 
@@ -21,11 +21,14 @@ export default {
       minify: true,
     }),
     /** Resolve bare module imports */
+    copy({
+      targets: [{ src: "lib/", dest: "dist" }],
+    }),
     nodeResolve(),
     /** Minify JS, compile JS to a lower language target */
     esbuild({
       minify: true,
-      target: ['chrome64', 'firefox67', 'safari11.1'],
+      target: ["chrome64", "firefox67", "safari11.1"],
     }),
     /** Bundle assets references via import.meta.url */
     importMetaAssets(),
@@ -33,9 +36,9 @@ export default {
     babel({
       plugins: [
         [
-          'babel-plugin-template-html-minifier',
+          "babel-plugin-template-html-minifier",
           {
-            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
+            modules: { lit: ["html", { name: "css", encapsulation: "style" }] },
             failOnError: false,
             strictCSS: true,
             htmlMinifier: {
